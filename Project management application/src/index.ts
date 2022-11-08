e268d85a3ac87f778477a9a7a2c39fab76fde102
Roman Troshin <roman.troshin98@gmail.com>
2022-11-08 22:37:21 +0300

    feat: add cluster connect

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,23 +1,22 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      "mongodb+srv://pug000:nwGr2jO1MKSlY3Fg@cluster0.90tlofk.mongodb.net/managerApp"
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
